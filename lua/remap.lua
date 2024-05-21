d48ae456ef6d29c9a4ac1205aa7ff38fba1b24eb
Lukas Eder <ederlukas@bluewin.ch>
2024-05-21 22:02:31 +0200

    implemented terminal remaps

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>fe', vim.cmd.Explore)
 vim.keymap.set('n', '<leader>pac', vim.cmd.PackerSync)
+vim.keymap.set('n', '<leader>t', vim.cmd.term)
 
 vim.keymap.set('n', '<leader>f', '10l')
 vim.keymap.set('n', '<leader>v', '10h')
+vim.keymap.set('t', '<leader>tt', '<C-\\><C-n>', { noremap = true, silent = true })
+
