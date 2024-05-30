5d3acf1f984716cadb11a0a724f64d1fa007e2db
Lukas Eder <ederlukas@bluewin.ch>
2024-05-30 19:56:20 +0200

    whitspaces

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,3 +21,16 @@ require('mason-lspconfig').setup({
  },
 })
 
+local cmp = require('cmp')
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		 -- confirm completion item
+    ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+
+    -- trigger completion menu
+    ['<C-c>'] = cmp.mapping.complete(),
+		
+		-- remaps
+		['<C-u>'] = cmp.mapping.select_prev_item({behavior = 'select'})
+	})
+})
